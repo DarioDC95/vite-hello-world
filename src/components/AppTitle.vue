aa91f6788b6bb62e324dcf74f0d54add75a210db
DarioDC95 <dario_95@live.com>
2023-01-10 16:18:17 +0100

    Exercise Done

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: 'AppTitle',
+    data() {
+        return {
+            message: 'La mia prima app con Vite!'
+        }
+    } 
+}
+</script>
+
+<template>
+    <div class="intro">
+        {{message}}
+    </div>
+</template>
+
+<style>
+    .intro {
+        font-size: 30px;
+        color: grey;
+        padding: 30px 45px;
+        background-color: #EEEEEE;
+        border-radius: 7px;
+    }
+</style>
